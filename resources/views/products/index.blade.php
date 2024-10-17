387b83f04be1824ddc9d554736b1508538a52539
sukanta555 <38782744+sukanta555@users.noreply.github.com>
2024-10-17 13:51:37 +0530

    Admin & User privilege
    Admin, User, & agent privilege on Add and edit portion.

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,7 +4,9 @@
 @section('contents')
     <div class="d-flex align-items-center justify-content-between">
         <h1 class="mb-0">List Product</h1>
-        <a href="{{ route('products.create') }}" class="btn btn-primary">Add Product</a>
+        @if (Auth::user()->is_admin === 'admin' || 'user')
+            <a href="{{ route('products.create') }}" class="btn btn-primary">Add Product</a>
+        @endif
     </div>
     <hr />
     @if(Session::has('success'))
@@ -23,7 +25,7 @@
                 <th>Action</th>
             </tr>
         </thead>
-        <tbody>+
+        <tbody>
             @if($product->count() > 0)
                 @foreach($product as $rs)
                     <tr>
@@ -35,19 +37,21 @@
                         <td class="align-middle">
                             <div class="btn-group" role="group" aria-label="Basic example">
                                 <a href="{{ route('products.show', $rs->id) }}" type="button" class="btn btn-secondary">Detail</a>
-                                <a href="{{ route('products.edit', $rs->id)}}" type="button" class="btn btn-warning">Edit</a>
-                                <form action="{{ route('products.destroy', $rs->id) }}" method="POST" type="button" class="btn btn-danger p-0" onsubmit="return confirm('Delete?')">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button class="btn btn-danger m-0">Delete</button>
-                                </form>
+                                @if (Auth::user()->is_admin === 'admin')
+                                    <a href="{{ route('products.edit', $rs->id)}}" type="button" class="btn btn-warning">Edit</a>
+                                    <form action="{{ route('products.destroy', $rs->id) }}" method="POST" class="btn btn-danger p-0" onsubmit="return confirm('Delete?')">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-danger m-0">Delete</button>
+                                    </form>
+                                @endif
                             </div>
                         </td>
                     </tr>
                 @endforeach
             @else
                 <tr>
-                    <td class="text-center" colspan="5">Product not found</td>
+                    <td class="text-center" colspan="6">Product not found</td>
                 </tr>
             @endif
         </tbody>
