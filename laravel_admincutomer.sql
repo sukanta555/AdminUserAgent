9e41f42b92bfba5b77c5c1d00b685991eace16cb
sukanta555 <38782744+sukanta555@users.noreply.github.com>
2024-10-21 23:54:58 +0530

    DB
    DB last work

--- laravel_admincutomer.sql
+++ laravel_admincutomer.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 17, 2024 at 08:34 AM
+-- Generation Time: Oct 21, 2024 at 08:24 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
@@ -58,7 +58,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (17, '2014_10_12_100000_create_password_reset_tokens_table', 1),
 (18, '2019_08_19_000000_create_failed_jobs_table', 1),
 (19, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(20, '2024_10_15_071105_create_products_table', 1);
+(20, '2024_10_15_071105_create_products_table', 1),
+(21, '2024_10_21_173307_add_soft_deletes_to_products_table', 2);
 
 -- --------------------------------------------------------
 
@@ -100,21 +101,28 @@ CREATE TABLE `personal_access_tokens` (
 CREATE TABLE `products` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `title` varchar(255) NOT NULL,
-  `price` varchar(255) NOT NULL,
+  `product_name` varchar(255) DEFAULT NULL,
+  `product_img` varchar(255) DEFAULT NULL,
+  `price` decimal(7,2) NOT NULL,
   `product_code` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `title`, `price`, `product_code`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'Electronic', '15000', 'Elc02', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,', '2024-10-16 02:47:36', '2024-10-16 02:47:36'),
-(2, 'Matching', '20000', 'Mg01', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English.', '2024-10-16 02:48:33', '2024-10-16 02:48:33'),
-(3, 'Furniture', '7000', 'FK01', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,', '2024-10-16 02:49:28', '2024-10-16 02:49:28');
+INSERT INTO `products` (`id`, `title`, `product_name`, `product_img`, `price`, `product_code`, `description`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Electronic', 'BURGUR', 'images/0uxsjv9avYy2rzGlP49Nxix6nUwFZ1dXWqKwkksN.jpg', 15000.00, 'Elc02', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,', '2024-10-16 02:47:36', '2024-10-21 03:03:22', NULL),
+(2, 'Matching', 'Bold BBQ Veggies', 'images/uGicjdt8YtBc7goY8geM2phpFJZmzlEWlG7KOU0a.jpg', 20000.00, 'Mg01', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English.', '2024-10-16 02:48:33', '2024-10-21 03:00:39', NULL),
+(3, 'Furniture', 'Margherita', 'images/Clow14wGZhkNzUMcBIt721tuiyg3y0cbO6eB0wRj.jpg', 7000.00, 'FK01', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,', '2024-10-16 02:49:28', '2024-10-21 03:00:19', NULL),
+(4, 'Domino\'s Pizza', 'Margherita', 'images/kTwMV03i8lJRuyEzlslbaDeymOpwlxWyKt5VTIvb.jpg', 549.99, 'DM01', 'Pizza topped with our herb-infused signature pan sauce and 100% mozzarella cheese. A classic treat for all cheese lovers out there! (PAN Per/Med-293 Kcal/100g |TnC-293 Kcal/100g)', '2024-10-19 02:20:34', '2024-10-21 02:55:36', NULL),
+(5, 'Pizza', 'Bold BBQ Veggies', 'images/4DWF1uNAqyGagqBHV6GxOHhxt6Ss7JBzvHCuWyfA.jpg', 350.00, 'BV02', 'Our signature pan sauce, with BBQ Sauce drizzle, topped with mushroom, onion, green capsicum, & red paprika (PAN Per/Med-242 Kcal/100g |TnC- 266 Kcal/100g)', '2024-10-19 04:42:30', '2024-10-21 12:15:22', '2024-10-21 12:15:22'),
+(6, 'Our burger', 'BURGUR', 'products/d3b6qJ9p4x11TGL51mztmqp9M5KORkZrpjzfvVZD.jpg', 160.00, 'BU01', 'Veggie Fajita Wrap · Veg Jalapeno Cheese Wrap · Veggie Paneer Wrap · Mexican Cottage Cheese Sandwich · BBQ Paneer Sandwich · Bombay Toast Sandwich', '2024-10-19 07:05:21', '2024-10-21 07:15:16', NULL),
+(7, 'test', 'BURGUR', 'images/PfkBsLnsB2A3Seft9a5hllAZnEckjLUQcqAKwVcI.jpg', 700.00, 'Elc02', 'test asfsadfdsa', '2024-10-21 03:06:54', '2024-10-21 12:14:57', '2024-10-21 12:14:57');
 
 -- --------------------------------------------------------
 
@@ -201,7 +209,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -213,7 +221,7 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
